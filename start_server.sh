0a84b4e4b4a2160a881d208815c58f35d9666073
rsperoni <rsperoni@mgcoders.com>
2018-01-29 18:20:36 -0300

    cambios calcium

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #/bin/bash
 set -x
-cd /home/ubuntu/wp-silicon-deploy
+cd /home/ubuntu/wp-calcium-deploy
 cp ../.env ./
 echo docker-compose up
 docker-compose build && docker-compose up -d
